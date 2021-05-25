224242a9558c1cea0dd7d1438143af827571e32d
1429879806@qq.com <1429879806@qq.com>
2021-05-25 15:23:25 +0800

    11.2diyi

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            global::System.Console.WriteLine(fef);
             return View();
         }
 
